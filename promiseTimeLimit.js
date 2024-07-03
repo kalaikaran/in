17dc7d686ea7c2ae43cfa51340edeac7981e9ebe
Kalaikaran Kalaidhasan <kalai8810@gmail.com>
2024-07-03 18:31:47 -0500

    few more

--- promiseTimeLimit.js
+++ promiseTimeLimit.js
@@ -2,12 +2,12 @@ var timeLimit = function(fn, t) {
   // const call = await fn(t);
   return async function(time) {
     console.log(time);
-      if(t <){
+  //     if(t <){
           
-      }
+  //     }
   }
 };
 
 
-  const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
- limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+limited(150).catch(console.log);
