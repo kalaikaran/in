448e4426fc099a41d2648eab9e95689185a5ce2c
Kalaikaran Kalaidhasan <kalai8810@gmail.com>
2024-09-24 14:19:52 -0500

    html

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-function testEvent() {
-  setTimeout(() => console.log("setTimeout"), 11);
-  new Promise((resolve) => setTimeout(() => resolve("resolve 1"), 10)).then(
-    (msg) => console.log(msg)
-  );
-  new Promise((resolve) => setTimeout(() => resolve("resolve 2"), 10)).then(
-    (msg) => console.log(msg)
-  );
-  new Promise((resolve) => setTimeout(() => resolve("resolve 3"), 10)).then(
-    (msg) => console.log(msg)
-  );
-  new Promise((resolve) => setTimeout(() => resolve("resolve 4"), 10)).then(
-    (msg) => console.log(msg)
-  );
-  new Promise((resolve) => setTimeout(() => resolve("resolve 5"), 10)).then(
-    (msg) => console.log(msg)
-  );
-}
-testEvent();
+var a = 5;
+var p = new Promise(function (resolve, reject) {
+  console.log(a, "promise");
+  a = 25;
+  resolve();
+});
+
+p.then(function () {
+  console.log(a, "promise.then");
+});
+
+console.log(a);
+
+setTimeout(function timeout() {
+  console.log(a, "timeout");
+  a = 10;
+}, 0);
+
+console.log(a);
